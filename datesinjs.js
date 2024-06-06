8337b3ec1b2bda2c5530807790224c87862460b1
keshvcode <info.keshavjangid@gmail.com>
2024-06-06 10:45:46 +0000

    dates learning done

--- datesinjs.js
+++ datesinjs.js
@@ -1,4 +1,5 @@
 let myDate  = new Date()
+
 console.log(myDate);
 console.log(myDate.toString());
 console.log(myDate.toLocaleString());
@@ -6,4 +7,22 @@ console.log(myDate.toLocaleDateString());
 console.log(typeof myDate);
 
 
+let newdate = new Date(2023 ,0,23 )
+console.log(newdate)
+console.log(newdate.getDate())
+console.log(newdate.getDay())
+
+let timestamp =  Date.now()
+console.log(Math.floor(Date.now()))
+
+let mydate = new Date()
+console.log(mydate.getDate());
+console.log(mydate.getDay());
+console.log(mydate.getHours());
+
+
+
+console.log(mydate.toLocaleString(`default`, {
+    weekday : "long"
+}));
 
