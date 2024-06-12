e3b1990455099c02843e36a951b4bf0a9774b451
keshvcode <info.keshavjangid@gmail.com>
2024-06-12 17:25:34 +0000

    array filter method

---
+++ iterator01.js
@@ -0,0 +1,13 @@
+const myarray = [0,1,2,3,4,5,6,7,8,9]
+
+// for (let i = 0; i < myarray.length; i++) {
+//     const element = myarray[i];
+//     console.log(element);
+    
+// }
+
+
+// let newarray = myarray.filter( (item) => item > 4 )
+const newarray2 = myarray.forEach( (item) => item > 4)
+
+console.log(newarray2);
